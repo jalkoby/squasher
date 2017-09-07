cbedd0ac88414a696819f8b3e79bb9edfaf0a9bb
Mikhail Pospelov <mixan946@yandex.ru>
2017-09-07 17:45:58 +0300

    Add sql schema support (#45)

--- lib/squasher/render.rb
+++ lib/squasher/render.rb
@@ -17,22 +17,48 @@ module Squasher
       ERB.new(template("#{ name }.rb"), nil, '-').result(binding)
     end
 
-    def each_schema_line
+    def each_schema_line(&block)
       File.open(config.schema_file, 'r') do |stream|
-        inside_schema = false
-        stream.each_line do |line|
-          if inside_schema
-            # reach the end of schema
-            break if line.index("end") == 0
-            yield line.gsub(/\A\s{,2}(.*)\s+\z/, '\1')
-          else
-            inside_schema = true if line.include?("ActiveRecord::Schema")
-          end
+        if @config.set?(:sql)
+          stream_structure(stream, &block)
+        else
+          stream_schema(stream, &block)
         end
       end
     end
 
     private
+
+    def stream_structure(stream)
+      yield 'execute <<-SQL'
+      skip_mode = false
+      ignored_table = ['ar_internal_metadata', 'schema_migrations']
+      stream.each_line do |line|
+        skip_mode = true if ignored_table.any? { |t| line.include?(t) }
+
+        if skip_mode
+          skip_mode = false if line.include?(';')
+          next
+        end
+
+        yield line.gsub(/\A\s{,2}(.*)\s+\z/, '\1')
+      end
+      yield 'SQL'
+    end
+
+    def stream_schema(stream)
+      inside_schema = false
+
+      stream.each_line do |line|
+        if inside_schema
+          # reach the end of schema
+          break if line.index("end") == 0
+          yield line.gsub(/\A\s{,2}(.*)\s+\z/, '\1')
+        else
+          inside_schema = true if line.include?("ActiveRecord::Schema")
+        end
+      end
+    end
 
     def template(name)
       path = File.join(File.dirname(__FILE__), "templates/#{ name }.erb")
--- spec/lib/worker_spec.rb
+++ spec/lib/worker_spec.rb
@@ -55,6 +55,64 @@ describe Squasher::Worker do
       Squasher.instance_variable_set(:@config, Squasher::Config.new)
     end
 
+    specify 'the sql mode' do
+      Squasher.config.set(:sql, true)
+      worker = described_class.new(Time.new(2014))
+      allow(worker).to receive(:under_squash_env).and_yield.and_return(true)
+      new_migration_path = File.join(Dir.tmpdir, 'init_schema.rb')
+      allow_any_instance_of(Squasher::Config).to receive(:migration_file).with('20131213090719', :init_schema).and_return(new_migration_path)
+      expect(FileUtils).to receive(:rm).with(File.join(fake_root, 'db', 'migrate', '20131205160936_first_migration.rb'))
+      expect(FileUtils).to receive(:rm).with(File.join(fake_root, 'db', 'migrate', '20131213090719_second_migration.rb'))
+
+      expect(Squasher).to receive(:ask).with(:keep_database).and_return(false)
+      expect(Squasher).to receive(:rake).with("db:drop")
+      expect(Squasher).to receive(:ask).with(:apply_clean).and_return(false)
+      worker.process
+
+      expect(File.exists?(new_migration_path)).to be_truthy
+      File.open(new_migration_path) do |stream|
+        content = stream.read
+        expect(content).to eq(<<-RUBY
+class InitSchema < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+    CREATE TABLE cities (
+      id integer NOT NULL,
+      name character varying,
+      created_at timestamp without time zone NOT NULL,
+      updated_at timestamp without time zone NOT NULL,
+    );
+    CREATE TABLE managers (
+      id integer NOT NULL,
+      email character varying,
+      password_digest character varying,
+      created_at timestamp without time zone NOT NULL,
+      updated_at timestamp without time zone NOT NULL
+    );
+    CREATE TABLE offices (
+      id integer NOT NULL,
+      name character varying,
+      address character varying,
+      phone character varying,
+      description text,
+      capacity integer,
+      manager_id integer,
+      city_id integer,
+      created_at timestamp without time zone NOT NULL,
+      updated_at timestamp without time zone NOT NULL
+    );
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, "The initial migration is not revertable"
+  end
+end
+        RUBY
+      )
+      end
+    end
+
     specify 'the dry mode' do
       Squasher.config.set(:dry, nil)
       worker = described_class.new(Time.new(2014))
